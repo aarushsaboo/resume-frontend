17655d8c7804dd16ce435910c545013a57f0c303
Aarush Saboo <160993404+aarushsaboo@users.noreply.github.com>
2025-01-25 19:52:10 +0530

    Working with the Organization component now.

---
+++ src/components/Organization/Organization.js
@@ -0,0 +1,11 @@
+import styles from './Organization.module.css';
+
+const Organization = () => {
+  return (
+    <div className={styles.organization}>
+      
+    </div>
+  )
+};
+
+export default Organization;
--- src/pages/ResumeWorkspace/ResumeWorkspace.js
+++ src/pages/ResumeWorkspace/ResumeWorkspace.js
@@ -6,9 +6,11 @@ const ResumeWorkspace = () => {
   return (
     <div className={styles.resumeWorkspace}>
       {/* before element.. new page provisions? */}
-          <div className={styles.resumeContent}>
-              <Personal />
-            <Heading />
+      <div className={styles.resumeContent}>
+        <Personal />
+        <div className={styles.education}>
+          <Heading />
+        </div>
       </div>
     </div>
   )
