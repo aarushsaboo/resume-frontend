4959bb7d97f382bc6883a9776413f4b84349c6c5
Aarush Saboo <160993404+aarushsaboo@users.noreply.github.com>
2025-01-30 06:32:07 +0530

    Made some headings editable. They're not minimal though. Gotta fix that next.

--- src/pages/ResumeWorkspace/components/Personal/Personal.js
+++ src/pages/ResumeWorkspace/components/Personal/Personal.js
@@ -1,20 +1,64 @@
+import { useInputWidth } from "../../../../hooks/useInputWidth"
 import styles from "./Personal.module.css"
 
-const Personal = ({leftIconStyles, rightIconStyles}) => {
+const Personal = ({ leftIconStyles, rightIconStyles }) => {
+  const {
+    value: nameText,
+    inputWidth: nameWidth,
+    handleInput: handleNameInput,
+    hiddenText: nameHiddenText,
+  } = useInputWidth({ initialValue: "Heramb pp" })
+
+  const {
+    value: detailsText,
+    inputWidth: detailsWidth,
+    handleInput: handleDetailsInput,
+    hiddenText: detailsHiddenText,
+  } = useInputWidth({
+    initialValue: "India | Phone # | heramb.ap@somaiya.edu | LinkedIn Link",
+  })
+
   return (
     <div className={styles.personal}>
-      <div className={styles.name}>
+      <div className={styles.nameContainer}>
         <div className={leftIconStyles}></div>
-        <p>
-          <strong>Heramb pp</strong>
-        </p>
-
+        <div className={styles.name}>
+          <input
+            type="text"
+            value={nameText}
+            onChange={handleNameInput}
+            className={styles.nameInput}
+            style={{ width: nameWidth }}
+          />
+          <span
+            ref={nameHiddenText}
+            className={styles.hiddenText}
+            aria-hidden="true"
+          >
+            {nameText}
+          </span>
+        </div>
         <div className={rightIconStyles}></div>
       </div>
       <div className={styles.container}>
-        <div className={styles.otherPersonalDetails}>
+        <div className={styles.otherPersonalDetailsContainer}>
           <div className={leftIconStyles}></div>
-          <p>India | Phone # | heramb.ap@somaiya.edu | LinkedIn Link</p>
+          <div className={styles.otherPersonalDetails}>
+            <input
+              type="text"
+              value={detailsText}
+              onChange={handleDetailsInput}
+              className={styles.detailsInput}
+              style={{ width: detailsWidth }}
+            />
+            <span
+              ref={detailsHiddenText}
+              className={styles.hiddenText}
+              aria-hidden="true"
+            >
+              {detailsText}
+            </span>
+          </div>
           <div className={rightIconStyles}></div>
         </div>
       </div>
